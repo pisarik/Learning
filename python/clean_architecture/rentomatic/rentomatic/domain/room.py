89d756c10e0a7e858bbcccdda5a9dacf3c112bd2
Vlad Blazhko <mr_plum@mail.ru>
2020-03-22 18:04:02 +0300

    Implement and test room

---
+++ python/clean_architecture/rentomatic/rentomatic/domain/room.py
@@ -0,0 +1,29 @@
+class Room:
+    def __init__(self, code, size, price, longitude, latitude):
+        self.code = code
+        self.size = size
+        self.price = price
+        self.longitude = longitude
+        self.latitude = latitude
+
+    @classmethod
+    def from_dict(cls, adict):
+        return cls(
+            code=adict['code'],
+            size=adict['size'],
+            price=adict['price'],
+            longitude=adict['longitude'],
+            latitude=adict['latitude'],
+        )
+
+    def to_dict(self):
+        return {
+            'code': self.code,
+            'size': self.size,
+            'price': self.price,
+            'longitude': self.longitude,
+            'latitude': self.latitude
+        }
+
+    def __eq__(self, other):
+        return self.to_dict() == other.to_dict()
