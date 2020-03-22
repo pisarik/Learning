d5c59bdbde5d1ffd878102fe8ecfe1cf473ba57b
Vlad Blazhko <mr_plum@mail.ru>
2020-03-22 22:03:26 +0300

    Fix: pass correct path to abspath and getsize

--- python/clean_architecture/tdd/fileinfo/fileinfo/fileinfo.py
+++ python/clean_architecture/tdd/fileinfo/fileinfo/fileinfo.py
@@ -12,6 +12,6 @@ class FileInfo:
         return (
             self.filename,
             self.original_path,
-            os.path.abspath(self.filename),
-            os.path.getsize(self.filename)
+            os.path.abspath(self.original_path),
+            os.path.getsize(self.original_path)
         )
