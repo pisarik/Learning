7e41379da12478528dea9b624ba1282deb48c88b
Vlad Blazhko <mr_plum@mail.ru>
2020-03-22 17:45:50 +0300

    Patch immutable objects

---
+++ python/clean_architecture/tdd/fileinfo/tests/test_logger.py
@@ -0,0 +1,14 @@
+from unittest.mock import patch
+
+from fileinfo.logger import Logger
+
+
+@patch('fileinfo.logger.datetime.datetime')
+def test_log(mock_datetime):
+    test_now = 123
+    test_message = 'A test message'
+    mock_datetime.now.return_value = test_now
+
+    test_logger = Logger()
+    test_logger.log(test_message)
+    assert test_logger.messages == [(test_now, test_message)]
