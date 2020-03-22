753ba12ccabf93d3d0267783f7aedd6d0303a815
Vlad Blazhko <mr_plum@mail.ru>
2020-03-22 16:47:49 +0300

    Learn patching

---
+++ python/clean_architecture/tdd/fileinfo/tests/test_fileinfo.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_fileinfo
+----------------------------------
+
+Tests for `fileinfo` module.
+"""
+
+import pytest
+from unittest.mock import patch
+
+from fileinfo.fileinfo import FileInfo
+
+
+def test_init():
+    filename = 'somefile.ext'
+    fi = FileInfo(filename)
+    assert fi.filename == filename
+
+
+def test_init_relative():
+    filename = 'somefile.ext'
+    relative_path = '../{}'.format(filename)
+    fi = FileInfo(relative_path)
+    assert fi.filename == filename
+
+
+def test_get_info():
+    filename = 'somefile.ext'
+    original_path = '../{}'.format(filename)
+    with patch('os.path.abspath') as abspath_mock:
+        test_abspath = 'some/abs/path'
+        abspath_mock.return_value = test_abspath
+        fi = FileInfo(original_path)
+        assert fi.get_info() == (filename, original_path, test_abspath)
